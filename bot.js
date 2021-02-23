e6eed8459f51653a4ba2a5e1dce8c45fbc2e839b
nicolas377 <programnicolas@gmail.com>
2021-02-22 21:02:56 -0500

    update

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var codes = []
-var users = []
+fs = require('fs')
 
 function randomString(length = 64) {
     var result = '';
@@ -12,23 +11,48 @@ function randomString(length = 64) {
     return result
 }
 
+function updateFiles() {
+  fs.writeFile('./json/codes.json', JSON.stringify(codes), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  })
+  fs.writeFile('./json/users.json', JSON.stringify(users), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  })
+}
+
+function readFiles() {
+  let rawcodes = fs.readFileSync('./json/codes.json')
+  codes = JSON.parse(rawcodes)
+
+  let rawusers = fs.readFileSync('./json/users.json')
+  users = JSON.parse(rawusers)
+}
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  readFiles()
+  console.log(`Ready`);
 });
 
 client.on('message', msg => {
   if (msg.content === '$code') {
-    str = randomString()
-    users.push(msg.author.id)
-    codes.push(str)
-    str = '```' + str + '```'
-    msg.author.send(str);
-    client.users.fetch('550456900861427733').then((user) => {
-    user.send("New Code: "+str);
-    });
-    console.log(users);
-    console.log(codes);
+    if(msg.member.roles.cache.find(r => r.name === "Election Boi")) {
+      str = randomString()
+      if (codes.includes(str)) {
+        delete(str)
+        str = randomString()
+      }
+      users.push(msg.author.id)
+      codes.push(str)
+      updateFiles()
+      str = '```' + str + '```'
+      msg.author.send(str);
+      console.log("New code created")
+    }
   }
 });
 
-client.login('NzgwNDU4MTIwNjA1OTkwOTU0.X7vYRA.4KACYErUrPt564RELap375DywQ8');
+client.login('NzgwNDU4MTIwNjA1OTkwOTU0.X7vYRA.QSlX3-cbPqrGfzSn5MxX1QOis8M');
