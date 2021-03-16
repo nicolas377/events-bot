e81f39298a1d69e8ceb06a8a56a6f4c644997d3b
PH-KDX <smtp.python.email.sender@gmail.com>
2021-03-16 14:28:09 +0100

    added gitignore, updated event-ideas channel id

--- source.js
+++ source.js
@@ -397,7 +397,7 @@ client.on('message', async (msg) => {
 			let member = msg.mentions.members.first()
 			if (member.roles.cache.some(role => role.name === 'Security Check')) {
 				member.roles.set(['553723642568114187'])
-				var message = `Welcome to GeoFS Events ${member}!\nWe hope you enjoy your stay!\nPlease make sure you have read <#553929583397961740> and <#553720929063141379>.\nWe organize and host events every day, so make sure to check <#756937922904850442> to keep up on events hosted for that day.\nThere are currently ${getMemberNumber(msg)} people in this server.\nIf you need any help or advice, contact the Elite Crew.\nUse <#717777238979903566> to put your event ideas in and we will do it as soon as possible!\n\nCheck out and subscribe to our channel here: <https://www.youtube.com/channel/UCZhJvrv8C6mb0FXENg6uh2w>`
+				var message = `Welcome to GeoFS Events ${member}!\nWe hope you enjoy your stay!\nPlease make sure you have read <#553929583397961740> and <#553720929063141379>.\nWe organize and host events every day, so make sure to check <#756937922904850442> to keep up on events hosted for that day.\nThere are currently ${getMemberNumber(msg)} people in this server.\nIf you need any help or advice, contact the Elite Crew.\nUse <#818115761843339274> to put your event ideas in and we will do it as soon as possible!\n\nCheck out and subscribe to our channel here: <https://www.youtube.com/channel/UCZhJvrv8C6mb0FXENg6uh2w>`
 				client.channels.cache.get('553718744657035274').send(message)
 				return msg.channel.send('User approved sucessfully')
 			}
