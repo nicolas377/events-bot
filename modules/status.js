4c083c228733eb12094b10940722a06ae0828f88
nrod06 <>
2021-04-11 17:49:00 +0000

    bugfix that status command

--- modules/status.js
+++ modules/status.js
@@ -1,5 +1,5 @@
 exports.status = function(msg) {
-	status = client.user.presence.activities[0].name
-	status = status.slice(0, -8)
-	return msg.channel.send('The current status is: `' + status + '`')
+	statustxt = client.user.presence.activities[0].name
+	statustxt = statustxt.slice(0, -8)
+	return msg.channel.send('The current status is: `' + statustxt + '`')
 }
