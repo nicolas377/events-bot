2d6a415f6ac0d7875681d32eae314d5343f2d8aa
nrod06 <>
2021-04-15 01:24:22 +0000

    switched channels for the welcome message, small changes

--- modules/approve.js
+++ modules/approve.js
@@ -7,7 +7,7 @@ exports.approve = function(msg) {
 		if (member.roles.cache.some(role => role.name === 'Security Check')) {
 			member.roles.set(['553723642568114187'])
 			var message = `Welcome to GeoFS Events ${member}!\nWe hope you enjoy your stay!\nPlease make sure you have read <#553929583397961740> and <#553720929063141379>.\nWe organize and host events every day, so make sure to check <#756937922904850442> to keep up on events hosted for that day.\nThere are currently ${memberNumber(msg)} people in this server.\nIf you need any help or advice, contact the Elite Crew.\nUse <#818115761843339274> to put your event ideas in and we will do it as soon as possible!\n\nCheck out and subscribe to our channel here: <https://www.youtube.com/channel/UCZhJvrv8C6mb0FXENg6uh2w>`
-			client.channels.cache.get('553718744657035274').send(message)
+			client.channels.cache.get('809619658814652436').send(message)
 			return msg.channel.send('User approved sucessfully')
 		}
 		return msg.channel.send("That didn't work.")
