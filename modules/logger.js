ba3616f9444387794020a2143f2e629355b95d71
nrod06 <>
2021-04-16 12:29:41 +0000

    small changes

--- modules/logger.js
+++ modules/logger.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 exports.logger = function(txt, log = false) {
-	var date = new Date(Date.now()).toUTCString()
+	var date = Date.now()
 	fs.appendFileSync('log.txt', `\n${date}: ${txt}`)
 	if (log) {
 		console.log(txt)
